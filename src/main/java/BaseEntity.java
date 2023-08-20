10b5e7b76ff027b0e7cbda96b449fc645d961c5d
Chiptune93 <eoen012@gmail.com>
2023-08-20 23:24:06 +0900

    :rocket: MappedSuperClass

---
+++ src/main/java/BaseEntity.java
@@ -0,0 +1,44 @@
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+//@Entity 특정 엔티티가 extends 를 통해 다른 클래스를 참조하는 경우 @Entity 가 오거나 @MappedSuperclass 있어야 한다.
+@MappedSuperclass
+public abstract class BaseEntity {
+    private String createdBy;
+    private LocalDateTime createdDate;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
--- src/main/java/RunApplication.java
+++ src/main/java/RunApplication.java
@@ -2,6 +2,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class RunApplication {
 
@@ -11,6 +12,16 @@ public class RunApplication {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Member member = new Member();
+            member.setName("test");
+            member.setCreatedBy("test");
+            member.setCreatedDate(LocalDateTime.now());
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
