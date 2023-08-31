45bd033028ac2fcaa9a7a0f33c59b21e26a60bfa
Chiptune93 <eoen012@gmail.com>
2023-08-31 18:53:53 +0900

    :construction:

--- src/main/java/RunApplication.java
+++ src/main/java/RunApplication.java
@@ -1,7 +1,11 @@
+import domain.Member;
+import domain.Team;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class RunApplication {
 
@@ -11,6 +15,34 @@ public class RunApplication {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+            Team team = new Team();
+            team.setName("team1");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member m = em.find(Member.class, member.getId());
+
+            System.out.println("findMember -> " + m.getTeam().getClass());
+
+            System.out.println("=============");
+            m.getTeam().getName(); // 초기화
+            System.out.println("=============");
+
+            /* 즉시로딩의 문제점. */
+
+            List<Member> memberList = em.createQuery("select m from Member m", Member.class).getResultList();
+            // JPQL은 기본적으로 쿼리가 번역되기 때문에 ... 만약 여기서 조인 걸어서 팀까지 가져오면 상관 없음.
+            // sql -> select * from Member -> 어? 팀이 없네. 가져와야되네?
+            // sql 추가 -> select * from Team where team_id = member.team_id -> 멤버 개수 만큼 실행 됨.
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
---
+++ src/main/java/domain/Member.java
@@ -0,0 +1,70 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // @ManyToOne(fetch = FetchType.LAZY) // 멤버 클래스만 DB에서 조회하겠다. 지연로딩.
+    @ManyToOne(fetch = FetchType.EAGER) // 즉시 로딩 해서 사용하겠다.
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
