3dd2f07ef1003d5b47bf2ba314b98790f3e8e8ce
Chiptune93 <eoen012@gmail.com>
2023-08-31 15:07:08 +0900

    :rocket:

---
+++ src/main/java/Member.java
@@ -0,0 +1,69 @@
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
--- src/main/java/RunApplication.java
+++ src/main/java/RunApplication.java
@@ -11,6 +11,32 @@ public class RunApplication {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+            Member member = new Member();
+            member.setName("hello jpa proxy");
+
+            em.persist(member);
+            em.flush();
+            em.clear();
+
+            // 다시 시작.
+
+            // 기본적인 조회
+//            Member findMember = em.find(Member.class, 1L);
+            // em.getReference
+            Member findMember = em.getReference(Member.class, member.getId());
+            // 여기서 findmember를 찍어보면 "class Member$HibernateProxy$DRUTZbVi" 라고 나온다.
+            // 프록시에서 생성한 객체이기 때문에.
+            System.out.println("findMember => " + findMember.getClass());
+            System.out.println("findMember => " + findMember.getId()); // 여기서는 위에서 생성한 객체의 아이디니까 알 수 있음.
+            System.out.println("findMember => " + findMember.getName()); // 네임은 어디서 가져왔나? 조회해서 .. 쿼리가 날라감
+
+
+
+
+
+            // printMemberAndTeam(member);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -18,5 +44,17 @@ public class RunApplication {
             em.close();
         }
         emf.close();
+    }
+
+    public static void printMember(Member member) {
+        System.out.println("member = " + member.getName());
+    }
+
+    public static void printMemberAndTeam(Member member) {
+        String username = member.getName();
+        System.out.println("username = " + username);
+
+        Team team = member.getTeam();
+        System.out.println("team = " + team);
     }
 }
