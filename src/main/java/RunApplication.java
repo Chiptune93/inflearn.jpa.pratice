64161da32be63f0c3bac0e0f8adc064db2299165
Chiptune93 <eoen012@gmail.com>
2023-09-14 01:48:01 +0900

    :construction:

--- src/main/java/RunApplication.java
+++ src/main/java/RunApplication.java
@@ -2,6 +2,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class RunApplication {
 
@@ -11,6 +12,14 @@ public class RunApplication {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+            Member member = new Member();
+            member.setName("123");
+            member.setAddress(new Address("1", "2", "3"));
+            member.setPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
+
+            em.persist(member);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
