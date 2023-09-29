dccdc1e25fe98315eb66cda1deee4ec0934f6522
Chiptune93 <eoen012@gmail.com>
2023-09-29 14:37:56 +0900

    :construction:

--- src/main/java/RunApplication.java
+++ src/main/java/RunApplication.java
@@ -2,6 +2,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class RunApplication {
 
@@ -11,6 +15,37 @@ public class RunApplication {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+            // JPQL
+            String jpql = "select m From Member m where m.name like ‘%hello%'";
+            List<Member> result = em.createQuery(jpql, Member.class)
+                    .getResultList();
+
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+            //루트 클래스 (조회를 시작할 클래스)
+            Root<Member> m = query.from(Member.class);
+            //쿼리 생성 CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), “kim”)); List<Member> resultList = em.createQuery(cq).getResultList();
+
+
+            // Query DSL
+            //JPQL
+            //select m from Member m where m.age > 18
+            JPAFactoryQuery query = new JPAQueryFactory(em);
+            QMember m = QMember.member;
+            List<Member> list =
+                    query.selectFrom(m)
+                            .where(m.age.gt(18))
+                            .orderBy(m.name.desc())
+                            .fetch();
+
+
+            // Native SQL
+            String sql ="SELECT ID, AGE, TEAM_ID, NAME FROM MEMBER WHERE NAME = ‘kim’";
+            List<Member> resultList = em.createNativeQuery(sql, Member.class).getResultList();
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
