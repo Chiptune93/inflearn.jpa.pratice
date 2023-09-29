67f370e7167b5ea6a1c24dfc907a64ad049bc8a5
Chiptune93 <eoen012@gmail.com>
2023-09-29 14:43:47 +0900

    :construction:
    (cherry picked from commit 42e701d2f29242348209a3e0b4255f08710aa932)

---
+++ src/main/java/domain/Items.java
@@ -0,0 +1,76 @@
+package domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public abstract class Items extends InfoHistory {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEMS_ID")
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Categorys> categorys = new ArrayList<>();
+
+//    @OneToMany(mappedBy = "items")
+//    private List<OrdersItems> ordersItems = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "items")
+//    private List<CategoryItems> categorysList = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+//    public List<CategoryItems> getCategorysList() {
+//        return categorysList;
+//    }
+//
+//    public void setCategorysList(List<CategoryItems> categorysList) {
+//        this.categorysList = categorysList;
+//    }
+//
+//    public List<OrdersItems> getOrdersItems() {
+//        return ordersItems;
+//    }
+//
+//    public void setOrdersItems(List<OrdersItems> ordersItems) {
+//        this.ordersItems = ordersItems;
+//    }
+}
---
+++ src/main/java/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package domain;
+
+public enum OrderStatus {
+    WAIT, PROCESS, DELIVERY, FINISH
+}
